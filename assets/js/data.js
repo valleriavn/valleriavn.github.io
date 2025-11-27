d89402a0cbde87009a149513439e104e34933c7c
valleriavn <nkimverlie@gmail.com>
2025-11-27 19:39:43 +0800

    updated format and design

---
+++ assets/js/data.js
@@ -0,0 +1,28 @@
+const siteData = {
+  portfolio: [
+    {
+      title: "Apple Vision Pro Showcase",
+      description:
+        "Website showcasing Apple Vision Pro's features with responsive design and modern UI components.",
+      image: "assets/pics/sc-vision-pro-act4.png",
+      viewLink: "https://valleriavn.github.io/vision-pro-showcase/",
+      docLink: "https://github.com/valleriavn/vision-pro-showcase",
+    },
+    {
+      title: "New York City Showcase",
+      description:
+        "Digital guide to New York City highlighting famous landmarks and cultural destinations.",
+      image: "assets/pics/sc-nyc-act5.png",
+      viewLink: "https://valleriavn.github.io/new-york-showcase/",
+      docLink: "https://github.com/valleriavn/new-york-showcase",
+    },
+    {
+      title: "Stranger Things Cast Gallery",
+      description:
+        "Interactive gallery displaying Stranger Things characters with detailed profiles and animations.",
+      image: "assets/pics/GALLERY-stranger-things.png",
+      viewLink: "https://valleriavn.github.io/stranger-things-cast-gallery/",
+      docLink: "https://github.com/valleriavn/stranger-things-cast-gallery",
+    },
+  ],
+};
